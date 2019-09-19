42b7e28d60fde63d9550c1d8e53019bf56e21515
Klaudia <klaudia.ma.garnek@gmail.com>
2019-09-19 23:57:08 +0200

    login/register - basic implementation

--- server.py
+++ server.py
@@ -5,140 +5,179 @@ import socket
 import selectors
 import types
 
-HOST = '127.0.0.1'  
+HOST = '127.0.0.1'
 PORT = 8888
 
-logs = {}
+#logs = {}
 
 selector = selectors.DefaultSelector()
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    
-clients_addresses = {}
+clientsAddresses = {}
 
-def accept_connection(sock):
+def acceptConnection(sock):
     connection, addr = sock.accept()
     connection.setblocking(False)
-    print("Connection from ", addr)
+    print("(L) Connection from ", addr)
     data = types.SimpleNamespace(addr = addr, inb = b"", outb = b"")
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
     selector.register(connection, events, data = data)    
 
-    
-def service_connection(key, mask):
-    sock = key.fileobj  #osobny sock klienta?
-    #print("client socket: ", sock)
+def serviceConnection(key, mask):
+    sock = key.fileobj
     data = key.data
     if mask & selectors.EVENT_READ:
         recv_data = sock.recv(1024)
-        print("message from: ", data.addr)
+        print("(L) Message from: ", data.addr)
         if recv_data:
-            manage_message(recv_data, data)
+            manageMessage(recv_data, data)
         else:
-            print("Closing connection to ", data.addr)
+            print("(L) Closing connection to ", data.addr)
             selector.unregister(sock)
             sock.close()
 
-            
-def manage_message(data, client_data):
-    data = data.decode("utf-8")
-    flag = data[0]
-    data = data[1:]
-    if flag == "R": #register
-        register_client(data, client_data)
-    if flag == "L": #login
-        login_client(data, client_data)
-    if flag == "U": #update server
-        storing_logs(data, client_data)  
-    if flag == "S": #synchronize
-        synchronizing(data, client_data)
+
+def manageMessage(msg, senderData):
+    msg = msg.decode("utf-8")
+    msg_id = msg[0]
+    msg = msg[2:]
+
+    if msg_id == "0":
+        registerClient(msg, senderData)
+    if msg_id == "1":
+        verifyRegistration(msg, senderData)
+    if msg_id == "2":
+        loginClient(msg, senderData)
+    if msg_id == "3":
+        storeLogs(msg, senderData)
+    if msg_id == "4":
+        synchronizing(msg, senderData)
 #rejestracja -> tworzymy plik z logami dla danego klienta + mapujemy adres ip klienta z id klienta
 #logowanie -> mapujemy ip z id klienta
-#zerwanie polaczenia -> usuwamy z listy adresów klienta dane ip
+#zerwanie polaczenia -> usuwamy z listy adresów klienta dane ip ?
 
+def mapClientAddress(clientName, address):
+    clientsAddresses[address] = clientName    # mapujemy ip -> klient (jeden klient moze miec wiele ip)
 
-def register_client(data, client_data):
-    if isClientAlreadyRegistered(data):
-        print("registration denied") #TODO - zamknąć połączenie 
+def checkVerificationCode(msg): #todo - mądrzejsze sprawdzanie?
+    f = open("registeringClients.txt", "r")
+    lines = f.read().splitlines()
+    isVerified = False
+    if msg in lines:
+        isVerified = True
+    f.close()
+    return isVerified
+
+def addNewClient(msg, clientData):
+    mail, password, _ = msg.split(":")
+    f = open("registeredClients.txt", "a+")
+    f.write(mail + ":" + password + "\n")
+    f.close()
+    #todo - usuwanie z registeringClients.txt
+    mapClientAddress(mail, clientData.addr)
+
+    f = open(mail + ".txt", "w+")
+    f.close()
+
+def verifyRegistration(msg, sender_data):
+    if not checkVerificationCode(msg):
+        print("Verification code incorrect")
     else:
-        f = open("clients.txt", "a+")
-        f.write(data + "\n")
-        f.close()
-    
-        map_client_address(data, client_data.addr)
-        #clients_addresses[client_data.addr] = data    # mapujemy ip -> klient (jeden klient moze miec wiele ip)
-        #print("clients: ", clients_addresses)
+        addNewClient(msg, sender_data)
+    #sendRegistrationConfirmation() - todo
 
-        f = open(data + ".txt", "w+")
-        f.close()
-  
-def login_client(data, client_data):
-    if not isClientAlreadyRegistered(data):
-        print("no such client")  
+
+def isClientAlreadyRegistered(msg): #todo - sprawdzac tylko po mailu
+    f = open("registeredClients.txt", "r")
+    lines = f.read().splitlines()
+    if msg in lines:
+        return True
+    return False
+
+def verifyClientEmail(msg):
+    mail, _ = msg.split(":")
+    print("veryfying mail: " + mail + " - todo")
+
+def generateAndSaveVerificationCode(msg):
+    generatedCode = "generatedCode" #todo - generating
+    f = open("registeringClients.txt", "a+")
+    f.write(msg + ":" + generatedCode + "\n")
+    f.close()
+    return generatedCode
+
+def sendMailWithVerificationCode(msg, code, clientData):
+    print("sending code: " + code + " - todo")
+
+def registerClient(msg, clientData):
+    if isClientAlreadyRegistered(msg):
+        print("Client already registered")
     else:
-        map_client_address(data, client_data.addr)
- 
+        verifyClientEmail(msg)
+        code = generateAndSaveVerificationCode(msg)
+        sendMailWithVerificationCode(msg, code, clientData)
 
-def map_client_address(client_name, address):
-    clients_addresses[address] = client_name    # mapujemy ip -> klient (jeden klient moze miec wiele ip)
-    print("clients: ", clients_addresses)   
- 
-def isClientAlreadyRegistered(client_name):
-    f = open("clients.txt", "r")
-    print("client name:",client_name)
+
+def verifyPassword(clientLogin, clientPassword): #todo - ???
+    f = open("registeredClients.txt", "r")
     clients = f.read().splitlines()
-    print("clients: ", clients)
-    flag = False
-    if client_name in clients:
-        flag = True			
+    clients = dict(map(lambda s : s.split(':'), clients))
+
+    isPasswordCorrect = False
+    for login, password in clients.items():
+        if login == clientLogin:
+            if password != clientPassword:
+                print("incorrect password")
+            else:
+                isPasswordCorrect = True
     f.close()
-    return flag		
-            			
- 
-def storing_logs(data, client_data):
-    print("We've received some logs - we need to store them")
-    f = open(clients_addresses[client_data.addr] + ".txt", "a+")
-    f.write(data + "\n")
+    return isPasswordCorrect
+
+def loginClient(msg, sender_data):
+    login, password = msg.split(":")
+    if verifyPassword(login, password):
+        mapClientAddress(login, sender_data.addr)
+        #sendLoginConfirmation() - todo
+
+
+def storeLogs(logs, clientData):  #todo - zabezpieczenie przed niezalogowanymi klientami
+    f = open(clientsAddresses[clientData.addr] + ".txt", "a+")
+    f.write(logs + "\n")
     f.close()
 
     
-def synchronizing(data, client_data):
-    print("We will synchronize - sending new logs to client")
-    current_timestamp = int(data)
-    print("timestamp: ", current_timestamp)
-    f = open(clients_addresses[client_data.addr] + ".txt", "r")
-    logs = f.readlines()
-    for log in logs:
-        timestamp = int(log[0:10])
-        log_data = log[10:]
-        if timestamp > current_timestamp:
-            send_data = bytes(log, 'utf-8')
+def synchronizing(msg, clientData):
+    synchTimestamp = int(msg)
+    f = open(clientsAddresses[clientData.addr] + ".txt", "r")
+    lines = f.readlines()
+    for line in lines:
+        timestamp, logs = line.split(":")
+        timestamp = int(timestamp)
+        #timestamp = int(log[0:10])
+        #log_data = log[10:]
+        if timestamp > synchTimestamp:
+            #sendData = bytes(logs, 'utf-8')
             #s.sendall(data2)
-            print("send_data: ", send_data)
-            print("address: ", client_data.addr)
+            #print("send_data: ", sendData)
+            #print("address: ", clientData.addr)
             #sock.sendto(send_data, client_data.addr)
-            print("should send log: ", log_data)
-
-    
+            print("should send log: ", logs)
 
             
 if __name__=='__main__':    
     sock.bind((HOST, PORT))
-    #print("server socket: ", socket.fileno())
     sock.listen()
     sock.setblocking(False)
     selector.register(sock, selectors.EVENT_READ)
-    print("Ready for connection")
+    print("(L) Ready for connection")
     
     while True:
         try:
             events = selector.select(timeout = None)
             for key, mask in events:
                 if key.data is None:
-                    accept_connection(key.fileobj)
+                    acceptConnection(key.fileobj)
                 else:
-                    service_connection(key, mask)
+                    serviceConnection(key, mask)
         except KeyboardInterrupt:
             selector.close()
             sys.exit()                
-                    
